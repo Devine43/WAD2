1fef8031110a4a916f43768140da517c91ade2a6
John devine <John devine>
2017-01-24 19:42:43 +0000

    Created first template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner <br/> <a href='/rango/about/'>About</a>")
+   # Construct a dictionary to pass the template engine as its context
+   # Note they key boldmessage is the same as {{ boldmessage }} in the template!
+   context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 
+   #Return a rendered response to send to the client
+   #We make use of the shortcut function to make our lives easier
+   #Note that the first parameter is the template we wish to use
+   return render(request, 'rango\index.html',context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page <br/> <a href ='/rango/'>Index</a>")
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#Generates the path for the templates directory
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
