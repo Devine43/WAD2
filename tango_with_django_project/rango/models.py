91ae3d91b08221d7aeb284e4433bc35484ab1f69
John devine <John devine>
2017-01-26 18:41:09 +0000

    Populated Database for the first time

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self): # For Python 2, use __unicode__ too
         return self.name
