7b5a54850db8761d39ad263d687eb6275c28a30f
John devine <John devine>
2017-01-26 13:55:37 +0000

    Migrated Database for the first time

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self): # For Python 2, use __unicode__ too
+        return self.title
+
+    def __unicode__(self):
+        return self.title
