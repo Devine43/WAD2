2e995e116e042ceeb8943a72ead2c5581b16c0d3
John devine <John devine>
2017-01-27 01:37:29 +0000

    Chapter 6 Without exercises

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -5,5 +5,8 @@ from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^about', views.about, name = 'about')
+    url(r'^about', views.about, name = 'about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
+
 ]
